17e110c698616503046955659a2cfb79bd637f13
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-11-06 21:54:09 -0300

    ValueDescriptor.tsx
    novas estilização

--- src/Components/Button/ValueDescriptor/ValueDescriptor.tsx
+++ src/Components/Button/ValueDescriptor/ValueDescriptor.tsx
@@ -8,8 +8,13 @@ export interface ValueDescriptorProps {
 }
 
 export default function ValueDescriptor(props: ValueDescriptorProps) {
+  const COLORS = {
+    primary: "#0099ff",
+    default: "#274060",
+  };
+
   return (
-    <VD.Wrraper>
+    <VD.Wrraper color={COLORS[props.color || "defaultF"]}>
       <span className="Description">{props.description}</span>
       <div>
         {props.isCurrency && <span className="Currency">{"R$"}</span>}
