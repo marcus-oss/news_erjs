dcfe8efeb4977ffde5606c6f88f09c8a591bca76
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-11-12 22:33:31 -0300

    Chart.stories.tsx

--- src/stories/Chart.stories.tsx
+++ src/stories/Chart.stories.tsx
@@ -1,6 +1,30 @@
 import { Meta, StoryObj } from "@storybook/react";
 import Chart, { ChartProps } from "../Components/Chart/chart";
 
+const data: Chart.ChartData = {
+  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
+  datasets: [
+    {
+      label: 'Receitas',
+      data: [500, 400, 600, 100, 800, 20],
+      fill: true,
+      backgroundColor: '#0099FF',
+      borderColor: '#0099FF',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
+    },
+    {
+      label: 'Despesas',
+      data: [100, 200, 250, 500, 1000, 600],
+      fill: true,
+      backgroundColor: '#274060',
+      borderColor: '#274060',
+      borderWidth: 0.5,
+      yAxisID: 'cashflow',
+    },
+  ],
+};
+
 const meta = {
   title: "Example/Chart",
   component: Chart,
@@ -12,5 +36,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    data,
+    title: "Média de performance nos últimos 12 meses",
+  },
+};
+
+export const WithOutData: Story = {
+  args: {
+    title: "Media de perfomance no ultimos 6 meses",
+  },
 };
