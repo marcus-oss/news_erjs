f6eccad8014f3c925072a66b0ceaaa1df1d0f3a2
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-11-13 22:12:05 -0300

    CircleChart.tsx

--- src/Components/CircleChart/CircleChart.tsx
+++ src/Components/CircleChart/CircleChart.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import * as CC from "./CircleChart.styles";
 
 export interface CircleChartsProps {
@@ -5,11 +6,52 @@ export interface CircleChartsProps {
   progress: number;
   caption?: string;
   theme?: "default" | "primary";
-  stokedWidth?: number;
+  strokedWidth?: number;
 }
 
 function CircleChart(props: CircleChartsProps) {
-  return <CC.Wrapper>todo:circle</CC.Wrapper>;
+  // função que recupera as cores do chart em base do tema
+  const getThemeColor = () => (props.theme === "primary" ? "#09f" : "#274060");
+  //setup configuração de cor
+  const THEME = getThemeColor();
+  const STROKE_WIDTH = props.strokedWidth || 8;
+  const STROKE_COLOR = props.strokedWidth || 8;
+
+  //parte matematica
+  const CENTER = props.size / 2;
+  const RADIUS = props.size / 2 - STROKE_WIDTH / 2;
+  const CIRCUMFERENCE = 2 * Math.PI * RADIUS;
+
+  //estado offset
+  const [offset, setOffset] = useState(CIRCUMFERENCE);
+
+  //obsevardor para o offset
+  useEffect(() => {
+    const progressOffset = ((100 - props.progress) / 100) * CIRCUMFERENCE;
+    setOffset(progressOffset);
+  }, [setOffset, CIRCUMFERENCE, props.progress, offset]);
+
+  return (
+    <CC.Wrapper>
+      <CC.SvgWrapper style={{ width: props.size, height: props.size }}>
+        <CC.Svg width={props.size} height={props.size}>
+          <CC.CircleBG cy={CENTER} cx={CENTER} r={RADIUS} />
+          <CC.Circle
+            fill="none"
+            cy={CENTER}
+            cx={CENTER}
+            r={RADIUS}
+            stroke={STROKE_COLOR}
+            strokeWidth={STROKE_WIDTH}
+            strokeDasharray={CIRCUMFERENCE}
+            strokeDashoffset={offset}
+          />
+        </CC.Svg>
+        <CC.Percentage>{props.progress}%</CC.Percentage>
+      </CC.SvgWrapper>
+      {props.caption && <CC.Caption>{props.caption}</CC.Caption>}
+    </CC.Wrapper>
+  );
 }
 
 export default CircleChart;
