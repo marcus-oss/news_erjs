d5217da9b2f414c39befd8deab91a52bb61c2efc
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-11-29 21:26:25 -0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './Views/Home.view';
+import NotFound404 from './Views/NotFound404.view';
+import Contact from './Views/Contact.view';
+import UserView from './Views/Home.view';
+import CalcView from './Views/CalcView';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+  ReactDOM.render(
+    
+    <React.StrictMode>
+      <div>
+      <nav>
+        <ul>
+          <li><a href="/Home">Home</a></li>
+        </ul>
+        <ul>
+          <li><a href="/Contato">Contato</a></li>
+        </ul>
+      </nav>
+      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path={'/home'} exact component={Home}/>
+          
+  
+          <Route path={'/Contato'} exact component={Contact}/>
+          
+
+          <Route path={'/Usuario/:userId'}  component={UserView}/>
+          <Route path={'/Calc/:a/:b'} component={CalcView} />
+  
+          <Route  exact component={NotFound404}/>
+  
+        </Switch>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+
