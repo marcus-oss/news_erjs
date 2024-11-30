cde7f6628bfc7e663c280efe20b684777ea7c8ba
Marcus Vinicius Faria <57508803+marcus-oss@users.noreply.github.com>
2024-11-29 21:28:35 -0300

    adicionando novas views  no projeto
    adicionando novas  rotas no projeto

---
+++ src/Views/Home.view.tsx
@@ -0,0 +1,5 @@
+export default  function  UserView(){
+    return <div>
+        <h1>Usuario</h1>
+    </div>
+}
---
+++ src/Views/NotFound404.view.tsx
@@ -0,0 +1,5 @@
+export default  function  NotFound404(){
+    return <div>
+        <h1>Não encontrado 404-Pagina não encotrada , revise o link da pagina!</h1>
+    </div>
+}
